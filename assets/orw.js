d221df63af18fa9a1a13c28b93172e370da15b31
Lorie Ransom <lorie@ransomdesign.com>
2015-09-21 09:53:53 -0700

    initial commit of orw work

---
+++ assets/orw.js
@@ -0,0 +1,11 @@
+  $(document).ready(function(){
+  	alert('The document is ready');
+  	// update already!
+  	// 
+    // $(slideshow).slick({
+    //   dots: true,
+    //   speed: 500
+    // });
+  });
+
+  	
---
+++ assets/orw2.js
@@ -0,0 +1,9 @@
+  $(document).ready(function(){
+
+    $(".slideshow").slick({
+      dots: true,
+      speed: 500
+    });
+  });
+
+  	
